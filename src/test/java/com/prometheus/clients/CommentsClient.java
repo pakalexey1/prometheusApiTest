44f08c89ab55af6d2187b82409d906582aa16412
pakalexey <mye604@gmail.com>
2025-09-05 19:44:26 -0400

    - added descriptions to PostsClient - created CommentsClients

--- src/test/java/com/prometheus/clients/CommentsClient.java
+++ src/test/java/com/prometheus/clients/CommentsClient.java
@@ -2,6 +2,7 @@ package com.prometheus.clients;
 
 import com.prometheus.core.BaseClient;
 import com.prometheus.core.RequestSpecs;
+import io.restassured.response.Response;
 
 public class CommentsClient extends BaseClient {
 
@@ -9,5 +10,35 @@ public class CommentsClient extends BaseClient {
 
     public CommentsClient(){
         super(RequestSpecs.defaultSpec());
+    }
+
+    //GET comment by id
+    public Response getComment(int id){
+        return get(COMMENTS + "/" + id);
+    }
+
+    //GET all comments
+    public Response listComments(){
+        return get(COMMENTS);
+    }
+
+    //GET comments for a post
+    public Response listCommentsForPost(int postId){
+        return get("/posts/" + postId + COMMENTS);
+    }
+
+    //CREATE/POST a comment
+    public Response createComment(Object body){
+        return post(COMMENTS, body);
+    }
+
+    //UPDATE/PUT a comment
+    public Response updateComment(int id, Object body){
+        return put (COMMENTS + "/" + id, body);
+    }
+
+    //DELETE a comment
+    public Response deleteComment(int id){
+        return delete (COMMENTS + "/" + id);
     }
 }
--- src/test/java/com/prometheus/clients/PostsClient.java
+++ src/test/java/com/prometheus/clients/PostsClient.java
@@ -13,25 +13,31 @@ public class PostsClient extends BaseClient {
         super(RequestSpecs.defaultSpec());
     }
 
+    // GET a post by ID
     public Response getPost(int id){
         return get(POSTS + "/" + id);
     }
-
+    // GET all posts
     public Response listPosts(){
-        return get(POSTS);}
+        return get(POSTS);
+    }
 
+    //GET posts by userID
     public Response listPostsByUser(int userId){
         return get(POSTS + "?userId=" + userId);
     }
 
+    //CREATE / POST a post
     public Response createPost(PostDto post){
         return post(POSTS, post);
     }
 
+    //UPDATE / PUT a post
     public Response updatePost(int id, PostDto post){
         return put(POSTS +"/" + id, post);
     }
 
+    //DELETE a post
     public Response deletePost(int id){
         return delete(POSTS + "/" + id);
     }
