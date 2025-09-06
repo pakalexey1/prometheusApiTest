b3f86b8d01bfc711047d6b68a3ddb579336354d2
pakalexey <mye604@gmail.com>
2025-09-06 08:52:00 -0400

    Created UsersClient

---
+++ src/test/java/com/prometheus/clients/UsersClient.java
@@ -0,0 +1,40 @@
+package com.prometheus.clients;
+
+import com.prometheus.core.BaseClient;
+import com.prometheus.core.RequestSpecs;
+import io.restassured.response.Response;
+
+public class UsersClient extends BaseClient {
+
+    private static final String USERS = "/users";
+
+    public UsersClient(){
+        super(RequestSpecs.defaultSpec());
+    }
+
+    //GET all users
+    public Response getAllUsers(){
+        return get(USERS);
+    }
+
+    //GET a user by an ID
+    public Response getUserById(int id){
+        return get(USERS + "/" + id);
+    }
+
+    //CREATE / POST a user
+    public Response createUser(Object body){
+        return post(USERS, body);
+    }
+
+    //UPDATE/ PUT a user
+    public Response updateUser(int id, Object body){
+        return put(USERS + "/" + id, body);
+    }
+
+    //DELETE a user
+    public Response deleteUSer(int id){
+        return delete(USERS + "/" +id);
+    }
+
+}
